cf8ba2d1d78356d2ad8623ddee86f5ebb8e9f3ea
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-07-01 22:27:53 +0100

    phase 2: resource not found error handler

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 require('express-async-errors');
 
 const { environment } = require('./config');
@@ -36,6 +37,34 @@ app.use(
 const routes = require('./routes');
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = 'Resource not found';
+    err.errors = ['The requested resource couldn\'t be found'];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = "Validation Error";
+    }
+
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title,
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
 
